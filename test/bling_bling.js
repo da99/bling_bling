2e58722f54b7ff87d898c48e227a1a13690b776d
da99 <i-hate-spam-1234567@mailinator.com>
2013-06-15 07:18:31 -0500

    Added: paragraph blocks

--- test/bling_bling.js
+++ test/bling_bling.js
@@ -7,7 +7,7 @@ describe( 'Section:', function () {
 
   it( 'turns sections into h3', function () {
     var p = BB.new("Section: Hello");
-    assert.equal("<h3>Hello</h3>", p.to_html());
+    assert.equal(p.to_html(), "<h3>Hello</h3>");
   });
 
 }); // === end desc
@@ -16,7 +16,7 @@ describe( 'Paragraphs/Blocks', function () {
 
   it( 'blocks of text into div.p', function () {
     var p = BB.new("\nHello\n");
-    assert.equal("<div class=\"p\">Hello</div>", p.to_html());
+    assert.equal(p.to_html(), "<div class=\"p\">Hello</div>" );
   });
 
 }); // === end desc
